f1d0d19c310f548f8fc993c269d437268ccd2403
Kenneth Brook <kenneth.brook@hotmail.com>
2022-12-23 10:54:38 -0500

    changed to fetch, no good

--- js/xml/xmlCall.js
+++ js/xml/xmlCall.js
@@ -1,9 +1,8 @@
-const Connect = new XMLHttpRequest();
 
-Connect.open("GET", "http://cs.simpleviewinc.com/feeds/listings.cfm?pagestart=1&apikey=ECB2A7C5-5056-A36A-1C4477A6EED8BB7B", false);
-Connect.setRequestHeader("Content-Type", "text/xml");
-Connect.send(null);
 
-const TheDocument = Connect.responseXML;
 
-console.log(TheDocument);
+fetch('https://cs.simpleviewinc.com/feeds/listings.cfm?pagestart=1&apikey=ECB2A7C5-5056-A36A-1C4477A6EED8BB7B')
+    // Handle success
+    .then(response => response.json())  // convert to json
+    .then(json => console.log(json))    //print data to console
+    .catch(err => console.log('Request Failed', err)); // Catch errors
